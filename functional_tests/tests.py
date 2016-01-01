abc8a895ddd98d18a10fb2cf1ca8947a2e8d8b93
Junsu <kandinell@gmail.com>
2016-01-01 14:58:12 +0900

    레이아웃과 스타일링을 위한 FT 첫 단계

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -16,7 +16,6 @@ class NewVisitorTest(LiveServerTestCase):
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
 
-
     def test_can_start_a_list_and_retrieve_it_later(self):
         # 에디스는 멋진 작업목록 온라인 앱이 나왓다는 소식을 듣고
         # 해당 웹사이트를 확인하러 간다.
@@ -84,4 +83,27 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotIn('공작깃털 사기', page_text)
         self.assertIn('우유사기', page_text)
 
-        # 만족하고 잠자리에 든다.
+    def test_layout_and_styling(self):
+        # 에디스는 메인 페이지를 방문한다
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024,768)
+
+        # 그녀는 입력 상자가 가운데 배치된 것을 본다
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+
+        # 그녀는 새로운 리스트를 시작하고 입력 상자가
+        # 가운데 배치된 것을 확인한다
+        inputbox.send_keys('testing\n')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+
+       # 만족하고 잠자리에 든다.
