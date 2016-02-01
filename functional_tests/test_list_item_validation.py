c01cc74f4c2a61c9ea0c397ceb507ec8920db7cb
Junsu <kandinell@gmail.com>
2016-02-01 21:55:45 +0900

    모델 계층의 중복 아이템 검증 구현

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -1,5 +1,6 @@
 from unittest import skip
 from .base import FunctionalTest
+from lists.forms import EMPTY_LIST_ERROR
 
 class ItemValidationTest(FunctionalTest):
 
@@ -12,7 +13,7 @@ class ItemValidationTest(FunctionalTest):
         # 페이지가 새로고침되고, 빈 아이템을 등록할 수 없다는
         # 에러 메시지가 표시된다
         error = self.browser.find_element_by_css_selector('.has-error')
-        self.assertEqual(error.text, "빈 아이템을 등록할 수 없습니다")
+        self.assertEqual(error.text, EMPTY_LIST_ERROR)
 
         # 다른 아이템을 입력하고 이번에는 정상 처리된다
         self.get_item_input_box().send_keys('우유 사기\n')
@@ -24,9 +25,23 @@ class ItemValidationTest(FunctionalTest):
         # 리스트 페이지에 다시 에러 메시지가 표시된다
         self.check_for_row_in_list_table('1: 우유 사기')
         error = self.browser.find_element_by_css_selector('.has-error')
-        self.assertEqual(error.text, "빈 아이템을 등록할 수 없습니다")
+        self.assertEqual(error.text,EMPTY_LIST_ERROR)
 
         # 아이템을 입력하면 정상 동작한다
         self.get_item_input_box().send_keys('tea 만들기\n')
         self.check_for_row_in_list_table('1: 우유 사기')
         self.check_for_row_in_list_table('2: tea 만들기')
+
+    def test_cannot_add_duplicate_items(self):
+        # 에디스는 메인 페이지로 돌아가서 신규 목록을 시작한다
+        self.browser.get(self.server_url)
+        self.get_item_input_box().send_keys('콜라 사기\n')
+        self.check_for_row_in_list_table('1: 콜라 사기')
+
+        # 실수로 중복 아이템을 입력한다.
+        self.get_item_input_box().send_keys('콜라 사기\n')
+
+        # 도움이 되는 에러 메시지를 본다.
+        self.check_for_row_in_list_table('1: 콜라 사기')
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(error.text, "이미 등록한 작업입니다.")
